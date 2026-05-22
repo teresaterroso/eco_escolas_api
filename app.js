4871b381038609f7a1a4e29ee223d7524a64165d
teresaterroso <teresazevedo118@gmail.com>
2026-05-22 17:44:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// create a simple API REST server with express for a generic route to test RASP communication with the WAF
+import express from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+    // log request verb, URL and body
+    console.log(`Received ${req.method} request to ${req.url} with body:`, req.body);
+    res.json({ message: 'Hello from the API!' });
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
+});
